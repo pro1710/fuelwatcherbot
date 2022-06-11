ee3a45806d94c36bc3bcc9679a619ec42cb4e5c6
rpykhniv <roman.pykhnivskyi@harmonicinc.com>
2022-06-11 19:59:37 +0300

    simple telegram bot

--- watcher/bot.py
+++ watcher/bot.py
@@ -1,1 +1,39 @@
 #placeholder
+
+import config
+
+import logging
+from datetime import datetime
+
+from telegram import Update
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update: Update, context: CallbackContext):
+    print(f'ECHO: {update.message.text}')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def main():
+    updater = Updater(token=config.TELEGRAM_BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
+
+    updater.stop()
+
+
+
+if __name__ == '__main__':
+    print(f'Started {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    try:
+        main()
+    except KeyboardInterrupt as kie:
+        print(f'Killed: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
